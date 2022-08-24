e6d1146302bea30299466fac738e7767f988ad30
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2022-08-23 23:19:25 -0300

    O usuario precisa digitar um nome válido para entrar na sala de chat. A conexão com API foi estabelecida é ela quem retorna se o nome é válido ou não

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-function hideModal(element) {
-  element.parentNode.classList.add("hidden");
+function hideModal() {
+  const modal = document.querySelector(".modal");
+  modal.classList.add("hidden");
+}
+
+function modalRequestValidName() {
+  const e = document.querySelector(".error-message");
+  e.innerText = "Nome em uso ou inválido! Digite um novo nome:";
 }
 
 function showSidebar() {
   const sidebar = document.querySelector("aside");
   sidebar.classList.toggle("open");
+}
+
+function getUserName() {
+  const userInput = document.querySelector(".modal input").value;
+  const userName = { name: userInput };
+
+  const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", userName);
+
+  promise.then(hideModal);
+  promise.catch(modalRequestValidName)
 }
