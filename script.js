125262727f747c4e3f790e2e8b669d77a60f6fd4
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2022-08-23 23:01:35 -0300

    Sidebar implementada no HTML e CSS

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function hideModal(element) {
   element.parentNode.classList.add("hidden");
 }
+
+function showSidebar() {
+  const sidebar = document.querySelector("aside");
+  sidebar.classList.toggle("open");
+}
