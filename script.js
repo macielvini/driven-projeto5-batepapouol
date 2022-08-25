5956e190a8dd2be2ff1dc3c3974a15d3d1c441b3
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2022-08-25 16:53:29 -0300

    Buscar mensagens concluido, agora as mensagens do servidor são exibidas na tela

--- script.js
+++ script.js
@@ -27,8 +27,11 @@ function showSidebar() {
   sidebar.classList.toggle("open");
 }
 
+getUserName();
+
 function getUserName() {
-  const userNameInput = document.querySelector(".modal input").value;
+  // const userNameInput = document.querySelector(".modal input").value;
+  const userNameInput = "vini1";
   userObj.name = userNameInput;
 
   const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", userObj);
@@ -36,6 +39,7 @@ function getUserName() {
   promise.then(hideModal);
   promise.catch(modalRequestValidName);
 
+  getMessages();
 
   // setInterval(isUserOnline, 5000);
 }
@@ -43,4 +47,35 @@ function getUserName() {
 function isUserOnline() {
   const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/status", userObj);
   promise.then(() => console.log("online"));
+}
+
+function getMessages() {
+  const promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
+  promise.then(displayMessages);
+}
+
+function displayMessages(response) {
+  const messages = response.data;
+  const chat = document.querySelector(".chat");
+
+  for (let i = 0; i < messages.length; i++) {
+
+    const from = messages[i].from;
+    let to = messages[i].to + ":";
+    const text = messages[i].text;
+    const type = messages[i].type;
+    const time = messages[i].time;
+
+    if (type === "status") to = "";
+
+    chat.innerHTML += `
+    <li class="message">
+      <span class="time">(${time})</span>
+      <span class="from">${from}</span>
+      <span class="to">${to}</span>
+      <span class="text">${text}</span>
+    </li>
+  `
+
+  }
 }
